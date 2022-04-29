c8e13cdfa100618222072815300c9cbf0224f75a
caioreigot <caiocrrodrigues2004@gmail.com>
2022-04-29 18:10:42 -0300

    Procfile removido

--- server.js
+++ server.js
@@ -120,6 +120,6 @@ io.on('connection', socket => {
     });
 });
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(port, () => {
     console.log(`Listening at ${host} on port ${port}`);
 });
